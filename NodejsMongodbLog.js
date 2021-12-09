4cc81a390dc85bb4ebaf33d3e3963da0e597d3a7
zff <2686150983@qq.com>
2021-12-08 17:34:15 -0800

    install mongoose successfully

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,5 +1,17 @@
 const express = require("express")
 const app = express()
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test1');
+const schema = {
+    name: String,
+    age: Number,
+    health: String,
+    hobby: String
+}
+const Cat = mongoose.model('cat1', schema);
+const kitty = new Cat({ name: 'testZildjian' });
+kitty.save().then(() => console.log('meow!'));
+
 app.use('/',express.static('public'))
 app.get("/input", (req, res) => {
     res.send(req.query)
